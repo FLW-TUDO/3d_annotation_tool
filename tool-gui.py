a2eecf46b3f30a5c947817ede98f7e8617b9c8a3
Anas Gouda <anas.gouda@tu-dortmund.de>
2022-04-13 10:49:19 +0200

    remove scene lighting controls

--- tool-gui.py
+++ tool-gui.py
@@ -48,154 +48,23 @@ class AnnotationScene:
 
 class Settings:
     UNLIT = "defaultUnlit"
-    LIT = "defaultLit"
-    NORMALS = "normals"
-    DEPTH = "depth"
-
-    DEFAULT_PROFILE_NAME = "Bright day with sun at +Y [default]"
-    POINT_CLOUD_PROFILE_NAME = "Cloudy day (no direct sun)"
-    CUSTOM_PROFILE_NAME = "Custom"
-    LIGHTING_PROFILES = {
-        DEFAULT_PROFILE_NAME: {
-            "ibl_intensity": 45000,
-            "sun_intensity": 45000,
-            "sun_dir": [0.577, -0.577, -0.577],
-            # "ibl_rotation":
-            "use_ibl": True,
-            "use_sun": True,
-        },
-        "Bright day with sun at -Y": {
-            "ibl_intensity": 45000,
-            "sun_intensity": 45000,
-            "sun_dir": [0.577, 0.577, 0.577],
-            # "ibl_rotation":
-            "use_ibl": True,
-            "use_sun": True,
-        },
-        "Bright day with sun at +Z": {
-            "ibl_intensity": 45000,
-            "sun_intensity": 45000,
-            "sun_dir": [0.577, 0.577, -0.577],
-            # "ibl_rotation":
-            "use_ibl": True,
-            "use_sun": True,
-        },
-        "Less Bright day with sun at +Y": {
-            "ibl_intensity": 35000,
-            "sun_intensity": 50000,
-            "sun_dir": [0.577, -0.577, -0.577],
-            # "ibl_rotation":
-            "use_ibl": True,
-            "use_sun": True,
-        },
-        "Less Bright day with sun at -Y": {
-            "ibl_intensity": 35000,
-            "sun_intensity": 50000,
-            "sun_dir": [0.577, 0.577, 0.577],
-            # "ibl_rotation":
-            "use_ibl": True,
-            "use_sun": True,
-        },
-        "Less Bright day with sun at +Z": {
-            "ibl_intensity": 35000,
-            "sun_intensity": 50000,
-            "sun_dir": [0.577, 0.577, -0.577],
-            # "ibl_rotation":
-            "use_ibl": True,
-            "use_sun": True,
-        },
-        POINT_CLOUD_PROFILE_NAME: {
-            "ibl_intensity": 60000,
-            "sun_intensity": 50000,
-            "use_ibl": True,
-            "use_sun": False,
-            # "ibl_rotation":
-        },
-    }
-
-    DEFAULT_MATERIAL_NAME = "Polished ceramic [default]"
-    PREFAB = {
-        DEFAULT_MATERIAL_NAME: {
-            "metallic": 0.0,
-            "roughness": 0.7,
-            "reflectance": 0.5,
-            "clearcoat": 0.2,
-            "clearcoat_roughness": 0.2,
-            "anisotropy": 0.0
-        },
-        "Metal (rougher)": {
-            "metallic": 1.0,
-            "roughness": 0.5,
-            "reflectance": 0.9,
-            "clearcoat": 0.0,
-            "clearcoat_roughness": 0.0,
-            "anisotropy": 0.0
-        },
-        "Metal (smoother)": {
-            "metallic": 1.0,
-            "roughness": 0.3,
-            "reflectance": 0.9,
-            "clearcoat": 0.0,
-            "clearcoat_roughness": 0.0,
-            "anisotropy": 0.0
-        },
-        "Plastic": {
-            "metallic": 0.0,
-            "roughness": 0.5,
-            "reflectance": 0.5,
-            "clearcoat": 0.5,
-            "clearcoat_roughness": 0.2,
-            "anisotropy": 0.0
-        },
-        "Glazed ceramic": {
-            "metallic": 0.0,
-            "roughness": 0.5,
-            "reflectance": 0.9,
-            "clearcoat": 1.0,
-            "clearcoat_roughness": 0.1,
-            "anisotropy": 0.0
-        },
-        "Clay": {
-            "metallic": 0.0,
-            "roughness": 1.0,
-            "reflectance": 0.5,
-            "clearcoat": 0.1,
-            "clearcoat_roughness": 0.287,
-            "anisotropy": 0.0
-        },
-    }
 
     def __init__(self):
         self.mouse_model = gui.SceneWidget.Controls.ROTATE_CAMERA
         self.bg_color = gui.Color(1, 1, 1)
-        self.show_skybox = False
         self.show_axes = False
-        self.use_ibl = True
-        self.use_sun = True
-        self.new_ibl_name = None  # clear to None after loading
-        self.ibl_intensity = 45000
-        self.sun_intensity = 45000
-        self.sun_dir = [0.577, -0.577, -0.577]
-        self.sun_color = gui.Color(1, 1, 1)
 
         self.apply_material = True  # clear to False after processing
         self._materials = {
-            Settings.LIT: rendering.MaterialRecord(),
             Settings.UNLIT: rendering.MaterialRecord(),
-            Settings.NORMALS: rendering.MaterialRecord(),
-            Settings.DEPTH: rendering.MaterialRecord()
         }
-        self._materials[Settings.LIT].base_color = [0.9, 0.9, 0.9, 1.0]
-        self._materials[Settings.LIT].shader = Settings.LIT
         self._materials[Settings.UNLIT].base_color = [0.9, 0.9, 0.9, 1.0]
         self._materials[Settings.UNLIT].shader = Settings.UNLIT
-        self._materials[Settings.NORMALS].shader = Settings.NORMALS
-        self._materials[Settings.DEPTH].shader = Settings.DEPTH
 
         # Conveniently, assigning from self._materials[...] assigns a reference,
         # not a copy, so if we change the property of a material, then switch
         # to another one, then come back, the old setting will still be there.
-        self.material = self._materials[Settings.LIT]
+        self.material = self._materials[Settings.UNLIT]
 
     def set_material(self, name):
         self.material = self._materials[name]
@@ -222,9 +91,9 @@ class AppWindow:
 
     DEFAULT_IBL = "default"
 
-    MATERIAL_NAMES = ["Lit", "Unlit", "Normals", "Depth"]
+    MATERIAL_NAMES = ["Unlit"]
     MATERIAL_SHADERS = [
-        Settings.LIT, Settings.UNLIT, Settings.NORMALS, Settings.DEPTH
+        Settings.UNLIT
     ]
 
     def _apply_settings(self):
@@ -233,7 +102,6 @@ class AppWindow:
             self.settings.bg_color.blue, self.settings.bg_color.alpha
         ]
         self._scene.scene.set_background(bg_color)
-        self._scene.scene.show_skybox(self.settings.show_skybox)
         self._scene.scene.show_axes(self.settings.show_axes)
         if self.settings.new_ibl_name is not None:
             self._scene.scene.scene.set_indirect_light(
@@ -241,36 +109,13 @@ class AppWindow:
             # Clear new_ibl_name, so we don't keep reloading this image every
             # time the settings are applied.
             self.settings.new_ibl_name = None
-        self._scene.scene.scene.enable_indirect_light(self.settings.use_ibl)
-        self._scene.scene.scene.set_indirect_light_intensity(
-            self.settings.ibl_intensity)
-        sun_color = [
-            self.settings.sun_color.red, self.settings.sun_color.green,
-            self.settings.sun_color.blue
-        ]
-        self._scene.scene.scene.set_sun_light(self.settings.sun_dir, sun_color,
-                                              self.settings.sun_intensity)
-        self._scene.scene.scene.enable_sun_light(self.settings.use_sun)
 
         if self.settings.apply_material:
             self._scene.scene.update_material(self.settings.material)
             self.settings.apply_material = False
 
-        self._bg_color.color_value = self.settings.bg_color
-        self._show_skybox.checked = self.settings.show_skybox
         self._show_axes.checked = self.settings.show_axes
-        self._use_ibl.checked = self.settings.use_ibl
-        self._use_sun.checked = self.settings.use_sun
-        self._ibl_intensity.int_value = self.settings.ibl_intensity
-        self._sun_intensity.int_value = self.settings.sun_intensity
-        self._sun_dir.vector_value = self.settings.sun_dir
-        self._sun_color.color_value = self.settings.sun_color
-        self._material_prefab.enabled = (
-                self.settings.material.shader == Settings.LIT)
-        c = gui.Color(self.settings.material.base_color[0],
-                      self.settings.material.base_color[1],
-                      self.settings.material.base_color[2],
-                      self.settings.material.base_color[3])
+        c = gui.Color(self.settings.material.base_color[0])
         self._material_color.color_value = c
         self._point_size.double_value = self.settings.material.point_size
 
@@ -295,13 +140,12 @@ class AppWindow:
         self.settings.new_ibl_name = resource_path + "/" + AppWindow.DEFAULT_IBL
 
         self.window = gui.Application.instance.create_window(
-            "Open3D", width, height)
+            "BOP manual annotation tool", width, height)
         w = self.window  # to make the code more concise
 
         # 3D widget
         self._scene = gui.SceneWidget()
         self._scene.scene = rendering.Open3DScene(w.renderer)
-        self._scene.set_on_sun_direction_changed(self._on_sun_dir)
 
         # ---- Settings panel ----
         # Rather than specifying sizes in pixels, which may vary in size based
@@ -329,129 +173,15 @@ class AppWindow:
         # of properties they rarely use.
         view_ctrls = gui.CollapsableVert("View controls", 0.25 * em,
                                          gui.Margins(em, 0, 0, 0))
-        view_ctrls.set_is_open(False)
-
-        self._arcball_button = gui.Button("Arcball")
-        self._arcball_button.horizontal_padding_em = 0.5
-        self._arcball_button.vertical_padding_em = 0
-        self._arcball_button.set_on_clicked(self._set_mouse_mode_rotate)
-        self._fly_button = gui.Button("Fly")
-        self._fly_button.horizontal_padding_em = 0.5
-        self._fly_button.vertical_padding_em = 0
-        self._fly_button.set_on_clicked(self._set_mouse_mode_fly)
-        self._model_button = gui.Button("Model")
-        self._model_button.horizontal_padding_em = 0.5
-        self._model_button.vertical_padding_em = 0
-        self._model_button.set_on_clicked(self._set_mouse_mode_model)
-        self._sun_button = gui.Button("Sun")
-        self._sun_button.horizontal_padding_em = 0.5
-        self._sun_button.vertical_padding_em = 0
-        self._sun_button.set_on_clicked(self._set_mouse_mode_sun)
-        self._ibl_button = gui.Button("Environment")
-        self._ibl_button.horizontal_padding_em = 0.5
-        self._ibl_button.vertical_padding_em = 0
-        self._ibl_button.set_on_clicked(self._set_mouse_mode_ibl)
-        view_ctrls.add_child(gui.Label("Mouse controls"))
-        # We want two rows of buttons, so make two horizontal layouts. We also
-        # want the buttons centered, which we can do be putting a stretch item
-        # as the first and last item. Stretch items take up as much space as
-        # possible, and since there are two, they will each take half the extra
-        # space, thus centering the buttons.
-        h = gui.Horiz(0.25 * em)  # row 1
-        h.add_stretch()
-        h.add_child(self._arcball_button)
-        h.add_child(self._fly_button)
-        h.add_child(self._model_button)
-        h.add_stretch()
-        view_ctrls.add_child(h)
-        h = gui.Horiz(0.25 * em)  # row 2
-        h.add_stretch()
-        h.add_child(self._sun_button)
-        h.add_child(self._ibl_button)
-        h.add_stretch()
-        view_ctrls.add_child(h)
-
-        self._show_skybox = gui.Checkbox("Show skymap")
-        self._show_skybox.set_on_checked(self._on_show_skybox)
-        view_ctrls.add_fixed(separation_height)
-        view_ctrls.add_child(self._show_skybox)
-
-        self._bg_color = gui.ColorEdit()
-        self._bg_color.set_on_value_changed(self._on_bg_color)
-
-        grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("BG Color"))
-        grid.add_child(self._bg_color)
-        view_ctrls.add_child(grid)
+        view_ctrls.set_is_open(True)
 
         self._show_axes = gui.Checkbox("Show axes")
         self._show_axes.set_on_checked(self._on_show_axes)
         view_ctrls.add_fixed(separation_height)
         view_ctrls.add_child(self._show_axes)
 
-        self._profiles = gui.Combobox()
-        for name in sorted(Settings.LIGHTING_PROFILES.keys()):
-            self._profiles.add_item(name)
-        self._profiles.add_item(Settings.CUSTOM_PROFILE_NAME)
-        self._profiles.set_on_selection_changed(self._on_lighting_profile)
-        view_ctrls.add_fixed(separation_height)
-        view_ctrls.add_child(gui.Label("Lighting profiles"))
-        view_ctrls.add_child(self._profiles)
         self._settings_panel.add_fixed(separation_height)
         self._settings_panel.add_child(view_ctrls)
-
-        advanced = gui.CollapsableVert("Advanced lighting", 0,
-                                       gui.Margins(em, 0, 0, 0))
-        advanced.set_is_open(False)
-
-        self._use_ibl = gui.Checkbox("HDR map")
-        self._use_ibl.set_on_checked(self._on_use_ibl)
-        self._use_sun = gui.Checkbox("Sun")
-        self._use_sun.set_on_checked(self._on_use_sun)
-        advanced.add_child(gui.Label("Light sources"))
-        h = gui.Horiz(em)
-        h.add_child(self._use_ibl)
-        h.add_child(self._use_sun)
-        advanced.add_child(h)
-
-        self._ibl_map = gui.Combobox()
-        for ibl in glob.glob(gui.Application.instance.resource_path +
-                             "/*_ibl.ktx"):
-            self._ibl_map.add_item(os.path.basename(ibl[:-8]))
-        self._ibl_map.selected_text = AppWindow.DEFAULT_IBL
-        self._ibl_map.set_on_selection_changed(self._on_new_ibl)
-        self._ibl_intensity = gui.Slider(gui.Slider.INT)
-        self._ibl_intensity.set_limits(0, 200000)
-        self._ibl_intensity.set_on_value_changed(self._on_ibl_intensity)
-        grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("HDR map"))
-        grid.add_child(self._ibl_map)
-        grid.add_child(gui.Label("Intensity"))
-        grid.add_child(self._ibl_intensity)
-        advanced.add_fixed(separation_height)
-        advanced.add_child(gui.Label("Environment"))
-        advanced.add_child(grid)
-
-        self._sun_intensity = gui.Slider(gui.Slider.INT)
-        self._sun_intensity.set_limits(0, 200000)
-        self._sun_intensity.set_on_value_changed(self._on_sun_intensity)
-        self._sun_dir = gui.VectorEdit()
-        self._sun_dir.set_on_value_changed(self._on_sun_dir)
-        self._sun_color = gui.ColorEdit()
-        self._sun_color.set_on_value_changed(self._on_sun_color)
-        grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("Intensity"))
-        grid.add_child(self._sun_intensity)
-        grid.add_child(gui.Label("Direction"))
-        grid.add_child(self._sun_dir)
-        grid.add_child(gui.Label("Color"))
-        grid.add_child(self._sun_color)
-        advanced.add_fixed(separation_height)
-        advanced.add_child(gui.Label("Sun (Directional light)"))
-        advanced.add_child(grid)
-
-        self._settings_panel.add_fixed(separation_height)
-        self._settings_panel.add_child(advanced)
 
         material_settings = gui.CollapsableVert("Material settings", 0,
                                                 gui.Margins(em, 0, 0, 0))
@@ -459,15 +189,7 @@ class AppWindow:
 
         self._shader = gui.Combobox()
         self._shader.add_item(AppWindow.MATERIAL_NAMES[0])
-        self._shader.add_item(AppWindow.MATERIAL_NAMES[1])
-        self._shader.add_item(AppWindow.MATERIAL_NAMES[2])
-        self._shader.add_item(AppWindow.MATERIAL_NAMES[3])
         self._shader.set_on_selection_changed(self._on_shader)
-        self._material_prefab = gui.Combobox()
-        for prefab_name in sorted(Settings.PREFAB.keys()):
-            self._material_prefab.add_item(prefab_name)
-        self._material_prefab.selected_text = Settings.DEFAULT_MATERIAL_NAME
-        self._material_prefab.set_on_selection_changed(self._on_material_prefab)
         self._material_color = gui.ColorEdit()
         self._material_color.set_on_value_changed(self._on_material_color)
         self._point_size = gui.Slider(gui.Slider.INT)
@@ -475,12 +197,6 @@ class AppWindow:
         self._point_size.set_on_value_changed(self._on_point_size)
 
         grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("Type"))
-        grid.add_child(self._shader)
-        grid.add_child(gui.Label("Material"))
-        grid.add_child(self._material_prefab)
-        grid.add_child(gui.Label("Color"))
-        grid.add_child(self._material_color)
         grid.add_child(gui.Label("Point size"))
         grid.add_child(self._point_size)
         material_settings.add_child(grid)
@@ -503,6 +219,7 @@ class AppWindow:
         # 3D Annotation tool options
         annotation_objects = gui.CollapsableVert("Annotation Objects", 0.33 * em,
                                                  gui.Margins(em, 0, 0, 0))
+        annotation_objects.set_is_open(True)
         self._meshes_available = gui.ListView()
         # mesh_available.set_items(["bottle", "can"])
         self._meshes_used = gui.ListView()
@@ -519,6 +236,7 @@ class AppWindow:
 
         scene_control = gui.CollapsableVert("Scene Control", 0.33 * em,
                                             gui.Margins(em, 0, 0, 0))
+        scene_control.set_is_open(True)
         pre_button = gui.Button("Previous")
         next_button = gui.Button("Next")
         pre_button.set_on_clicked(self._on_previous_scene)
